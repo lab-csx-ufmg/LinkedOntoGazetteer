16d1713c890b3e74923b5d8c703bb9f327b739f8
thvmm <thvaladares@gmail.com>
2015-04-25 00:43:07 -0300

    Skinning

--- LinkedOntoGazetteerWeb/src/br/ufmg/dcc/linkedontogazetteer/bean/SearchBean.java
+++ LinkedOntoGazetteerWeb/src/br/ufmg/dcc/linkedontogazetteer/bean/SearchBean.java
@@ -1,8 +1,11 @@
 package br.ufmg.dcc.linkedontogazetteer.bean;
 import java.util.ArrayList;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
+import br.ufmg.dcc.linkedontogazetteer.AppConfiguration;
 import br.ufmg.dcc.linkedontogazetteer.rexster.rest.api.GremlinRESTClient;
 import br.ufmg.dcc.linkedontogazetteer.rexster.rest.api.entity.Response;
 import br.ufmg.dcc.linkedontogazetteer.rexster.rest.api.entity.ResultObject;
@@ -10,7 +13,8 @@ import br.ufmg.dcc.linkedontogazetteer.view.VertexWrapper;
  
 @ManagedBean(name = "searchBean")
 public class SearchBean {
- 
+	private static final AppConfiguration config = AppConfiguration.getConfiguration();
+	
 	private String queryText;
 	private Double queryTime;
 	
@@ -19,10 +23,16 @@ public class SearchBean {
 	
 	
 	public SearchBean() {
-		this.client = new GremlinRESTClient("rexster", "MicroGEO@00");
+		this.client = new GremlinRESTClient(SearchBean.config.getRexsterUser(), SearchBean.config.getRexsterPassword());
 	}
 	
 	public void doQuery() {
+		if(this.queryText == null || this.queryText.isEmpty()) {
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Please, query a place name	",  null);
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+			return;
+		}
+		
 		Response response = this.client.getVerticesByName(this.queryText);
 		this.queryTime = response.getQueryTime();
 		System.out.println(response.getQueryTime());
